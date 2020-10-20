36ebf6afd26c9dfa6354825d20ddb60a63b6c77b
fredchen <fredchen1994@outlook.com>
2020-10-20 16:19:27 +0100

    add layout demo

---
+++ lib/layout_demo.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class LayoutDemoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(color, Icons.call, 'CALL'),
+              _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+              _buildButtonColumn(color, Icons.share, 'SHARE'),
+            ]
+        )
+    );
+
+    return MaterialApp(
+        title: "Flutter Layout Demo",
+        home: Scaffold(
+            appBar: AppBar(title: Text("Flutter Layout Demo")),
+            body: Column(
+              children: [
+                titleSection,
+                buttonSection
+              ],
+            )
+        )
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'random_words.dart';
+import 'layout_demo.dart';
 
 // one line function can use arrow notation
-void main() => runApp(RandomWordsApp());
+void main() => runApp(LayoutDemoApp());
