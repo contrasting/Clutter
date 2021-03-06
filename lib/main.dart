20386c7ab5a9a715033f92689e68b90e623d3f46
fredchen <fredchen1994@outlook.com>
2021-03-06 16:17:08 +0000

    add websockets app

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'routing.dart';
 import 'random_words.dart';
 import 'layout_demo.dart';
 import 'animation.dart';
+import 'websockets.dart';
 
 // https://stackoverflow.com/questions/59870357/how-to-remove-hash-from-url-in-flutter-web
 
 // one line function can use arrow notation
 void main() {
   configureApp();
-  runApp(Nav2App());
+  runApp(WebSocketsApp());
 }
